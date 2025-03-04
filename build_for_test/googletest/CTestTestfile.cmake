3321e4faaea658befe54d651eccf05f15c6a953e
garikmirakyan06 <garikmirakyan06@gmail.com>
2025-03-04 14:43:15 +0400

    Delete build_for_test directory

--- build_for_test/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/garik/Desktop/Vector-with-allocators-and-iterators/googletest
-# Build directory: /home/garik/Desktop/Vector-with-allocators-and-iterators/build_for_test/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
